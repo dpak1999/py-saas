060c491a4ddc2d9c6bf86f2ab333e3ee73b24383
deepak <deepakkumardash313@gmail.com>
2024-08-31 23:42:59 +0530

    initial commiy

---
+++ src/saashome/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import pathlib
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "total_visit": qs.count(),
+    }
+
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
